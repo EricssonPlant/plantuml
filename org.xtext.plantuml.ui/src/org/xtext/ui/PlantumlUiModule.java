d5e69c9b0dc1de3609037db9f83ec14a6cee7b22
unknown <eaonban@SE00202069.ericsson.se>
2015-06-12 12:54:13 +0200

    Fixed gitignore (?) Updated relevant files

--- org.xtext.plantuml.ui/src/org/xtext/ui/PlantumlUiModule.java
+++ org.xtext.plantuml.ui/src/org/xtext/ui/PlantumlUiModule.java
@@ -4,6 +4,7 @@
 package org.xtext.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.autoedit.AbstractEditStrategyProvider;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -11,5 +12,12 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class PlantumlUiModule extends org.xtext.ui.AbstractPlantumlUiModule {
 	public PlantumlUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+	
+	// Adds support for PlantUML comments.
+	// Removes autocompletion of brackets, parathesis and curly brackets.
+	@Override
+	public Class<? extends AbstractEditStrategyProvider> bindAbstractEditStrategyProvider() {
+		return QuotesAutoEditStrategy.class;
 	}
 }

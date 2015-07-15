562662099f48530565335866503a418f890cdd02
Anton Bergman <beanton@student.chalmers.se>
2015-07-15 11:13:19 +0200

    Fixed hexcode and color checking valueconverter

--- org.xtext.plantuml/src/org/xtext/PlantumlValueConverter.java
+++ org.xtext.plantuml/src/org/xtext/PlantumlValueConverter.java
@@ -10,57 +10,54 @@ import org.eclipse.xtext.util.Strings;
 public class PlantumlValueConverter extends DefaultTerminalConverters {
     boolean Exists = false;
     String[] colorArray = {
-                    "AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black"
-                    ,"BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse"
-                    ,"Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan"
-                    ,"DarkGoldenRod","DarkGray","DarkGreen","DarkGrey","DarkKhaki","DarkMagenta","DarkOliveGreen"
-                    ,"DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray"
-                    ,"DarkSlateGrey","DarkTurquoise","DarkViolet","Darkorange","DeepPink","DeepSkyBlue"
-                    ,"DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia"
-                    ,"Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Green","GreenYellow","Grey"
-                    ,"HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush"
-                    ,"LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow"
-                    ,"LightGray","LightGreen","LightGrey","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue"
-                    ,"LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen"
-                    ,"Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen"
-                    ,"MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue"
-                    ,"MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab"
-                    ,"Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed"
-                    ,"PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Psurple","Red","RosyBrown"
-                    ,"RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell"
-                    ,"Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen"
-                    ,"SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White"
-                    ,"WhiteSmoke","Yellow","YellowGreen"
+    		"aliceblue","antiquewhite","aqua","aquamarine","azure","beige","bisque","black"
+            ,"blanchedalmond","blue","blueviolet","brown","burlywood","cadetblue","chartreuse"
+            ,"chocolate","coral","cornflowerblue","cornsilk","crimson","cyan","carkblue","darkcyan"
+            ,"darkgoldenrod","darkgray","darkgreen","darkgrey","darkkhaki","darkmagenta","darkolivegreen"
+            ,"darkorchid","darkred","darksalmon","darkseagreen","darkslateblue","darkslategray"
+            ,"darkslategrey","darkturquoise","darkviolet","darkorange","deeppink","deepskyblue"
+            ,"dimgray","dimgrey","dodgerblue","firebrick","floralwhite","forestgreen","fuchsia"
+            ,"gainsboro","ghostwhite","gold","goldenrod","gray","green","greenyellow","grey"
+            ,"honeydew","hotpink","indianred","indigo","ivory","khaki","lavender","lavenderblush"
+            ,"lawngreen","lemonchiffon","lightblue","lightcoral","lightcyan","lightgoldenrodyellow"
+            ,"lightgray","lightgreen","lightgrey","lightpink","lightsalmon","lightseagreen","lightskyblue"
+            ,"lightslategray","lightslategrey","lightsteelblue","lightyellow","lime","limegreen","linen"
+            ,"magenta","maroon","mediumaquamarine","mediumblue","mediumorchid","mediumpurple","mediumseagreen"
+            ,"mediumslateblue","mediumspringgreen","mediumturquoise","mediumvioletred","midnightblue"
+            ,"mintcream","mistyrose","moccasin","navajowhite","navy","oldlace","olive","olivedrab"
+            ,"orange","orangered","orchid","palegoldenrod","palegreen","paleturquoise","palevioletred"
+            ,"papayawhip","peachpuff","peru","pink","plum","powderblue","psurple","red","rosybrown"
+            ,"royalblue","saddlebrown","salmon","sandybrown","seagreen","seashell"
+            ,"sienna","silver","skyblue","slateblue","slategray","slategrey","snow","springgreen"
+            ,"steelblue","tan","teal","thistle","tomato","turquoise","violet","wheat","white"
+            ,"whitesmoke","yellow","yellowgreen"
     };
     
     boolean found = false;
     
     @ValueConverter(rule = "Color")
     public IValueConverter<String> Color(){
-            return new IValueConverter<String>(){
+        return new IValueConverter<String>(){
 
-                    @Override
-                    public String toValue(String string, INode node) throws ValueConverterException{
-                    	// If colorArray does not contains string
-                    		if (org.eclipse.xtext.util.Arrays.contains(colorArray, string)) {
-                        			throw new ValueConverterException("Not a Color", node, null);
-                        	}
-                    		else return string;
-                    		
-                    		// TODO: Fix for hexcode
-                    	/*if(! string.matches("^\\s*[0-9A-Fa-f]*\\s*$")){
-                    		throw new ValueConverterException("Not a Color", node, null);
-                    	}
-                    	else return string;*/
-                    	
-                    }
+            @Override
+            public String toValue(String string, INode node) throws ValueConverterException{
+            	// If colorArray does not contains string
+            	if (!org.eclipse.xtext.util.Arrays.contains(colorArray, string.toLowerCase())
+            			&& !string.toLowerCase().matches("^\\s*[0-9a-f]*\\s*$")) {
+            			throw new ValueConverterException("Not a Color: " + string, node, null);
+	            	}
 
-                    @Override
-                    public String toString(String value)
-                                    throws ValueConverterException {
-                            // Auto-generated method stub
-                            return value;
-                    }              
-            };     
+            	else{
+        			return string;
+        		}
+            }
+
+            @Override
+            public String toString(String value)
+                            throws ValueConverterException {
+                    // Auto-generated method stub
+                    return value;
+            }              
+        };
     }
 }

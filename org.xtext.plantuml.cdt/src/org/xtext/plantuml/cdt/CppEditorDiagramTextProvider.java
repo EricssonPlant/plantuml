45c8935c33fa4861a50349867cea8ef15f1ffc3d
Anton Lundgren <anton.cg.lundgren@gmail.com>
2015-08-14 14:05:41 +0200

    Added additional comments to CppEditorDiagramTextProvider.java

--- org.xtext.plantuml.cdt/src/org/xtext/plantuml/cdt/CppEditorDiagramTextProvider.java
+++ org.xtext.plantuml.cdt/src/org/xtext/plantuml/cdt/CppEditorDiagramTextProvider.java
@@ -29,8 +29,7 @@ import org.eclipse.ui.IFileEditorInput;
 
 import net.sourceforge.plantuml.text.AbstractDiagramTextProvider;
 
-// this is just some basic experimenting! see JavaEditorDiagramTextProvider for inspiration
-// add this to net.sourceforge.plantuml.jdt - plugin.xml
+// See JavaEditorDiagramTextProvider for inspiration
 public class CppEditorDiagramTextProvider extends AbstractDiagramTextProvider {
 
 	private class Context {
@@ -42,15 +41,17 @@ public class CppEditorDiagramTextProvider extends AbstractDiagramTextProvider {
 	
 	private Context currentContext = null;
 	StringBuilder result = new StringBuilder();
-	//String namespaceName = "";
 	public boolean supportsSelection(ISelection selection) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 	
+	// This method will get the owner classes for a class and the namespaces this class may reside in
 	public StringBuilder getNamespacesAndClasses(IASTNode parentTraversalNode){
 		StringBuilder namespaceName = new StringBuilder();
 		StringBuilder ownerClassName = new StringBuilder();
+		// This loop traverses the parents of the parameter node while gathering information about the names of the
+		// namespaces and owner classes.
 		while(parentTraversalNode.getParent() != null){
     		parentTraversalNode = parentTraversalNode.getParent();
     		if(parentTraversalNode instanceof ICPPASTNamespaceDefinition){
@@ -62,7 +63,8 @@ public class CppEditorDiagramTextProvider extends AbstractDiagramTextProvider {
     	}
 		return namespaceName.append(ownerClassName);
 	}
-	
+	// This method is called when the method getDiagramText is run. It will make sure that the nodes of the AST that are 
+	// considered declarations are visited.
 	public void collectAllDeclarators(IASTTranslationUnit tu){
 		ASTVisitor visitor = new ASTVisitor(){
 			{ shouldVisitDeclarations = true;}
@@ -70,29 +72,25 @@ public class CppEditorDiagramTextProvider extends AbstractDiagramTextProvider {
 			@Override
 			public int visit(IASTDeclaration declaration){
 				
-				//This if-statement makes sure that only the declarations that contain classes will be searhed.
+				//This if-statement makes sure that only the declarations that contain classes will be searched.
 				if (declaration instanceof CPPASTSimpleDeclaration) {
 			        IASTDeclSpecifier specifier = ((CPPASTSimpleDeclaration) declaration).getDeclSpecifier();
+			        
+			        // If specifier is an instance of CPPASTCompositeTypeSpecifier, the node in the AST is a class and can have 
+			        // its name printed in the diagram.
 			        if(specifier instanceof CPPASTCompositeTypeSpecifier){
-			        	//This if-statement makes sure that owner classes and namespaces corresponding the the displayed class will be shown in the diagram.
-			        	
 			        	CPPASTCompositeTypeSpecifier compositeSpecifier = (CPPASTCompositeTypeSpecifier)specifier;
 			        	result.append("class " + getNamespacesAndClasses((IASTNode) declaration) + compositeSpecifier.getName().toString() +"{\n}\n");
-			        	//System.out.println("HERE -> "+ compositeSpecifier.getName().toString());
+			        	// The method GetBaseSpecifiers() returns the classes that the class in the current node inherits from.
 			        	ICPPASTCompositeTypeSpecifier.ICPPASTBaseSpecifier[] inheritance = compositeSpecifier.getBaseSpecifiers();
+			        	// This for loop will print the inherited class and the inheritor showing their relationship in the diagram
 			        	for(int i=0; i<inheritance.length; i++){
-			        		//System.out.println(inheritance[i].getNameSpecifier());
-			        		IASTNode parentTraversalNode = inheritance[i].getNameSpecifier();
-			        		while(parentTraversalNode.getParent() != null){
-			        			parentTraversalNode = parentTraversalNode.getParent();
-			        			System.out.println(parentTraversalNode.getRawSignature());
-			        			System.out.println("-----------------------------------");
-			        		}
 			        		result.append(inheritance[i].getNameSpecifier() + " <|.. " + getNamespacesAndClasses((IASTNode) declaration) + compositeSpecifier.getName().toString() + "\n");
 			        		
 			        	}
 			        }
 			    }
+				// This return value indicates that it's ok to traverse the children nodes.
 				return PROCESS_CONTINUE;
 			}
 		};
@@ -102,15 +100,16 @@ public class CppEditorDiagramTextProvider extends AbstractDiagramTextProvider {
 		
 	}
 	
+	// This method provides the diagram returns the text that is to be displayed in the Plantuml diagram.
 	@Override
 	protected String getDiagramText(IEditorPart editorPart, IEditorInput editorInput, ISelection selection) {
 		result.setLength(0);
-		// step 1: Check if this is a cpp file
+		// Checks if this is a '.h' file.
 		if (! (editorInput instanceof IFileEditorInput && ("h".equals(((IFileEditorInput) editorInput).getFile().getFileExtension()) 
 		|| "cpp".equals(((IFileEditorInput) editorInput).getFile().getFileExtension())))) {
 			return null;
 		}
-		// step 2: get file?
+		// Gets file
 		IFile sourceFile = ((IFileEditorInput) editorInput).getFile();
 		currentContext = new Context();
 		
@@ -120,8 +119,11 @@ public class CppEditorDiagramTextProvider extends AbstractDiagramTextProvider {
 		currentContext.translationUnit = CoreModelUtil.findTranslationUnit(currentContext.project.getFile(sourceFile.getProjectRelativePath()));
 		
 		try {
+			// Using the indexed AST means we can use the setting AST_SKIP_ALL_HEADERS to not have included files in our created AST
 			IIndex index= CCorePlugin.getIndexManager().getIndex(currentContext.iCProject);
 			try {
+				// When using an indexed AST it is recommended to acquire the ReadLock before doing something to the tree.
+				// I'm not sure of how relevant it is for our scenario but I did it anyway just to be on the safe side.
 				index.acquireReadLock();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
@@ -130,22 +132,28 @@ public class CppEditorDiagramTextProvider extends AbstractDiagramTextProvider {
 			
 			
 			try {
+				// The AST is created based off of the translation unit. With the parameter value AST_SKIP_ALL_HEADERS we skip the header files
+				// when creating our AST tree.
 				currentContext.iastTranslationUnit = currentContext.translationUnit.getAST(index, ITranslationUnit.AST_SKIP_ALL_HEADERS);
 				collectAllDeclarators(currentContext.iastTranslationUnit);
 			} catch (CoreException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			} finally {
+				// When done using the AST, the ReadLock should be released
 				index.releaseReadLock();
 				currentContext.iastTranslationUnit = null;
 			}
 		} catch (CoreException e) {
 			System.err.println(e);
 		} finally {
+			// Resetting the context
 			currentContext = null;
 		}
+		// If the file being shown in the editor is a '.cpp' file, this message will be displayed in the plantuml diagram 
+		// to alert the user about the lack of support for '.cpp' files.
 		if("cpp".equals(((IFileEditorInput) editorInput).getFile().getFileExtension())){
-			return "\nLegend left \nDiagrams will only be displayed for .h files. \nendlegend \n@enduml";
+			return "\nLegend left \nDiagrams will only be displayed for '.h' files. \nendlegend \n@enduml";
 		}
 		return result.toString();
 	}
